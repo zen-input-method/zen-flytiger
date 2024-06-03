a35477d995918168436762a9508be95c90160e62
pimgeek <pimgeek@outlook.com>
2024-06-03 11:59:33 +0800

    对 README.md 的修改：
    * 从原来的 67880 个常用词组变成 535912 个常用词组
    * 去掉顶字上屏的功能说明 (非主要功能，暂不强调)
    * 英文长句输入模式支持回车键退出
    
    对 lua/mixed_input.lua 的修改：
    
    * engine 缩写为 eng，context 缩写为 ctx
    * 增加英文长句的回车键响应代码
    * 为 `~!` 类单一符号输入响应代码增加注释文字
    
    对 rime.lua 的修改：
    
    * 重构 pimgeek_preedit_filter，分情况处理各类输入
      先考虑 `;xx` 快速符号输入，再考虑 `^[A-Z]` 短英文输入
      第三考虑英文长句输入，第四考虑单字和词组输入
      需要进一步考虑 3 码、4 码、5-6 码三种细分情况
    
    对 shared_emoji.txt 的修改：
    
    * 增加了 ✪，😅，😌，🤠，🥲，🥹，😵‍💫，🙏🏻，🐈‍⬛，「，」等符号
    
    对 zen-flytiger.dict.yaml 的修改：
    
    * zen-flytiger 输入码表版本号改为 v0.0.5
    * 重新为编码的各分区编号，⓿ 对应个性化编码
      ❶ 对应 1 简字，❷ 对应 2 简字，❸ 对应 3 简字
      ❹ 对应全码字，❺ 对应词组和短语
    * 增加自定义词组 “其它”，作为形码定词的示范
    * 把原来的自制词库替换为雾凇拼音的基础词库
    
    对 zen-flytiger.schema.yaml 的修改：
    
    * zen-flytiger 输入方案版本号改为 v0.0.5
    * 不再突出顶字上屏的功能特色
    * 默认关闭自造词功能
    * 默认关闭单字候选优先显示功能 ("yige 埸,一个" 不再优先显示 “埸”)
    * 默认关闭编码提示功能
    * shared_emoji 候选列表关闭评论显示
    * `` ` `` 反查功能明确提示 “虎码反查”
    * 关闭 = 计算式功能
    * 默认启用 inline_preedit 功能

--- squirrel-for-mac/lua/mixed_input.lua
+++ squirrel-for-mac/lua/mixed_input.lua
@@ -1,23 +1,33 @@
 local function mixed_input(key, env)
-  local engine = env.engine
-  local context = engine.context
-	local input = context.input
+  local eng = env.engine
+  local ctx = eng.context
+  local input = ctx.input
   local kRejected, kAccepted, kNoop = 0, 1, 2
-  if (context:is_composing() and input:sub(1, 1) == "~") then
+  if (ctx:is_composing() and input:sub(1, 1) == "~") then
     if (key:repr() == "Return") then
       if (input == "~") then
-        context.input = "~"
+        ctx.input = "~"
+      ---- 英文长句，遇回车键自动上屏 ----
       else
-        context.input = input:gsub("^~", ""):gsub("`"," ")
+        ctx.input = input:gsub("`"," ")
+        ctx:pop_input(#input)
+        eng:commit_text(input:gsub("^~", ""):gsub("`"," "))
+        return kAccepted
       end
     elseif (key:repr() == "space") then
+      -- 输入编码为 '~'，按空格后直接上屏 ~
       if (input == "~") then
-        context.input = "~~"
-      elseif (string.match(input, "^~[^!#(-&/:-@[-`{-~]")) then
-        context:push_input("`")
+        ctx.input = "~~"
+      -- 输入编码为 '~[^*]'，其中 * 为指定的单字符集合
+      -- 当第二位编码属于该字符集合时，后接空格将会直接上屏，
+      -- 对于其它字符，连按两次空格才上屏
+      -- 例如：" 通常成对出现，不希望它们单空格上屏
+      -- 但是 (、[、{ 因为可以触发双链笔记功能，所以希望单空格上屏
+      -- 这些符号的选择与日常打字习惯密切相关，可以逐步调整优化
+      elseif (string.match(input, "^~[^!#&(,./:;<=>?@[-^_{|}-]")) then
+        -- 必须在输入编码中额外插一个 `，否则无法退出临时英文输入模式
+        ctx:push_input("`")
         return kAccepted
-      elseif (string.match(input, "``")) then
-        context.input = input:gsub("``", "")
       end
     end
   end
--- squirrel-for-mac/rime.lua
+++ squirrel-for-mac/rime.lua
@@ -46,18 +46,120 @@ function pimgeek_preedit_filter(input, env)
   local eng = env.engine
   local ctx = eng.context
   local code = ctx.input
-  local cands = 0
-  for cand in input:iter() do
-    if (code == ";rq" or code == ";zd" or code == ";zt" or code == ";zz") then
-      eng:commit_text(cand.text)
-      ctx:pop_input(#code)
-    elseif (#code>=1) then
-      yield(cand)
+  local dict = eng.schema.schema_id
+  local mem = Memory(eng, eng.schema)
+  ---- 处理快速符号类编码 ; ----
+  if (code:sub(1,1) == ";") then
+    for cand in input:iter() do
+      ---- 以下快速符号不用敲空格，直接上屏 ----
+      if (code == ";rq" or code == ";zd" or code == ";zt" or code == ";zz") then
+        ctx:pop_input(#code)
+        eng:commit_text(cand.text)
+      ---- 其它快速符号，先预上屏，然后敲空格确认 ----
+      else
+        cand.preedit = cand.text
+        yield(cand)
+      end
     end
-  end
-  if (cands == 1) then
-    local selcand = ctx:get_selected_candidate()
-    eng:commit_text(selcand.text)
-    ctx:pop_input(#code)
+  ---- 处理首字母大写的英文单词类编码 ^[A-Z] ----
+  elseif string.match(code:sub(1,1),"[A-Z]") then
+    local tmpcand = Candidate("capital-word", 0, #code, code, "🔤")
+    yield(tmpcand)
+  ---- 处理英文长句类编码 ~ ----
+  elseif (code:sub(1,1) == "~") then
+    for cand in input:iter() do
+      ---- 输出英文句子 ----
+      if (code:sub(#code-1,#code) == "``") then
+        ctx:pop_input(#code)
+        eng:commit_text(cand.text:sub(1, #cand.text-1))
+      else
+        yield(cand)
+      end
+    end
+  ---- 处理单字和词组类编码 ----
+  else
+    if (#code <= 3) then
+      for cand in input:iter() do
+        cand.preedit = cand.text
+        yield(cand)
+      end
+    elseif (#code == 4) then
+      --local committed = false
+      for cand in input:iter() do
+        local c4code_words = {}
+        local c4code_wdmap = {}
+        local c4code_wdcnt = 0
+        mem:dict_lookup(code, true, 5)
+        for entry in mem:iter_dict() do
+          table.insert(c4code_words, entry.text)
+          c4code_wdmap[entry.text] = 1
+        end
+        for wd in pairs(c4code_wdmap) do
+          c4code_wdcnt = c4code_wdcnt + 1
+        end
+        ---- 确定是唯一候选，可直接上屏 ----
+        if (c4code_wdcnt==1) then
+          tmpcand = Candidate("temp", 0, #code, c4code_words[1], "唯一候选")
+          tmpcand.preedit = c4code_words[1]
+          yield(tmpcand)
+          --if (committed == false) then
+          --  ctx:pop_input(#code)
+          --  eng:commit_text(cand.text)
+          --  committed = true
+          --end
+        ---- 候选不唯一，可在末尾加 * 号标记 ----
+        else
+          cand.preedit = cand.text -- .. "*"
+          yield(cand)
+          for _, wd in ipairs(c4code_words) do
+            if (wd ~= cand.text) then
+              tmpcand = Candidate("temp", 0, #code, wd, "") -- 其它候选
+              tmpcand.preedit = wd
+              yield(tmpcand)
+            end
+          end
+        end
+      end
+    elseif (#code >= 5) then
+      local tmpcand = ""
+      ---- 获取最末位编码 ----
+      local newcode = code:sub(#code, #code) 
+      local fullcode_words = {}
+      local fullcode_candtext = ""
+      ---- 用完整编码查候选 ----
+      mem:dict_lookup(code, true, 5)
+      for entry in mem:iter_dict() do
+        tmpcand = Candidate("temp", 0, #code, entry.text, "") -- 空码检测
+        tmpcand.preedit = entry.text
+        yield(tmpcand)
+        table.insert(fullcode_words, entry.text)
+      end
+      for _, wd in ipairs(fullcode_words) do
+        fullcode_candtext = fullcode_candtext .. wd
+      end
+      ---- 确认当前编码是空码 ----
+      if (fullcode_candtext:len() == 0) then
+        ---- 去掉最末位编码，上屏首选 ----
+        local prevcode_words = {}
+        mem:dict_lookup(code:sub(1, #code-1), true, 5)
+        for entry in mem:iter_dict() do
+          table.insert(prevcode_words, entry.text)
+        end
+        ctx:pop_input(#code)
+        eng:commit_text(prevcode_words[1])
+        ---- 最末位编码单独输出 ----
+        local newcode = code:sub(#code, #code) -- 获取最末位编码
+        local lastcode_words = {}
+        local lastcode_words_checked = {}
+        local lastcode_words_count = 0
+        ctx:push_input(newcode)
+        mem:dict_lookup(newcode, true, 5)
+        for entry in mem:iter_dict() do
+          tmpcand = Candidate("temp", 0, 1, entry.text, "") -- 末位编码
+          tmpcand.preedit = entry.text
+          yield(tmpcand)
+        end
+      end
+    end
   end
 end
